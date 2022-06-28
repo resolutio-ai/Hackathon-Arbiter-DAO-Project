ae6c67a8f45b046affe070277179ca0b3a793709
Alexandra ogubuike <ogubuikealex@gmail.com>
2022-06-28 07:12:10 +0100

    Adding the ovting contract integration

---
+++ Integration/Implementations/Voting.js
@@ -0,0 +1,9 @@
+import { ethers } from "ethers";
+
+import {
+    disputePoolAddress
+} from '../config';
+
+import {voting} from "../Artifacts/contracts/VotingTwo.sol/VotingTwo.json";
+
+async function createDispute(url) {}
