d012b949baa31902630d94bc19479a4b7e07034b
Bestin John <bestinjohn1993@gmail.com>
2022-08-15 02:11:17 +0200

    Added GA tag

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -1,18 +1,36 @@
 import { Box, Container } from "@mui/material";
+import Script from "next/script";
 import AppFooter from "./AppFooter";
 import AppHeader from "./AppHeader";
 import ScrollToTop from "./ScrollToTop";
 
 const Layout = ({ children }) => {
   return (
-    <Box className="root">
-      <AppHeader />
-      <Container component="main" className="content">
-        <ScrollToTop />
-        {children}
-      </Container>
-      <AppFooter />
-    </Box>
+    <>
+      <Script
+        strategy="lazyOnload"
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID}`}
+      />
+
+      <Script strategy="lazyOnload" id="ga-script">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID}', {
+          page_path: window.location.pathname,
+          });
+        `}
+      </Script>
+      <Box className="root">
+        <AppHeader />
+        <Container component="main" className="content">
+          <ScrollToTop />
+          {children}
+        </Container>
+        <AppFooter />
+      </Box>
+    </>
   );
 };
 
